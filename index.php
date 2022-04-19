48ae612c973fbdb741c00d923499cb0d4819c784
Johann <joe7hann@gmail.com>
2022-04-19 03:58:44 -0500

    done

--- index.php
+++ index.php
@@ -8,36 +8,37 @@
       <!-- MESSAGES -->
 
       <?php if (isset($_SESSION['message'])) { ?>
-      <div class="alert alert-<?= $_SESSION['message_type']?> alert-dismissible fade show" role="alert">
-        <?= $_SESSION['message']?>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <?php session_unset(); } ?>
+        <div class="alert alert-<?= $_SESSION['message_type'] ?> alert-dismissible fade show" role="alert">
+          <?= $_SESSION['message'] ?>
+          <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+      <?php session_unset();
+      } ?>
 
       <!-- ADD TASK FORM -->
       <div class="card card-body">
         <form action="save_task.php" method="POST">
           <div class="form-group">
             Nombres: <input type="text" name="nombres" class="form-control" placeholder="Ingrese nombre" autofocus>
-		<br>
-	    DNI: <input type="text" name="dni" class="form-control" placeholder="Ingrese DNI" autofocus>
-		<br>
-	    ID de apuesta:<input type="text" name="id_apuesta" class="form-control" placeholder="Ingrese ID apuesta" autofocus>
-		<br>
-    _____________________________________________
-    <br>
-	    <strong>Detalles de la apuesta:</strong>
-		<br>
-	    Ganancia Total: <input type="text" name="valor_apostado" class="form-control" placeholder="Ingrese valor apostado" autofocus>
-		<br>
-	    Cuotas:<input type="text" name="cuota" class="form-control" placeholder="Ingrese Cuota" autofocus>
-		<br>
-	    Pila de apuestas: <input type="text" name="pila_apuesta" class="form-control" placeholder="Ingrese cantidad de combinadas" autofocus>
+            <br>
+            DNI: <input type="text" name="dni" class="form-control" placeholder="Ingrese DNI" autofocus>
+            <br>
+            ID de apuesta:<input type="text" name="id_apuesta" class="form-control" placeholder="Ingrese ID apuesta" autofocus>
+            <br>
+            _____________________________________________
+            <br>
+            <strong>Detalles de la apuesta:</strong>
+            <br>
+            Ganancia Total: <input type="text" name="valor_apostado" class="form-control" placeholder="Ingrese valor apostado" autofocus>
+            <br>
+            Cuotas:<input type="text" name="cuota" class="form-control" placeholder="Ingrese Cuota" autofocus>
+            <br>
+            Pila de apuestas: <input type="text" name="pila_apuesta" class="form-control" placeholder="Ingrese cantidad de combinadas" autofocus>
           </div>
 
-          
+
 
           <input type="submit" name="save_task" class="btn btn-success btn-block" value="Agregar participante">
         </form>
@@ -57,26 +58,34 @@
 
           <?php
           $query = "SELECT * FROM participante ORDER BY puntos DESC";
-          $result_tasks = mysqli_query($conn, $query);    
+          $result_tasks = mysqli_query($conn, $query);
 
-          while($row = mysqli_fetch_assoc($result_tasks)) { ?>
-          <tr>
-            <td><?php echo $row['dni']; ?></td>
-            <td><?php echo $row['nombres']; ?></td>
-            <td><?php echo $row['id_apuesta']; ?></td>
-	    <td><?php echo $row['puntos']; ?></td>
-            <td>
-              <a href="edit.php?id=<?php echo $row['dni']?>" class="btn btn-secondary">
-                <i class="fas fa-marker"></i>
-              </a>
-              <a href="delete_task.php?id=<?php echo $row['dni']?>" class="btn btn-danger">
-                <i class="far fa-trash-alt"></i>
-              </a>
-            </td>
-          </tr>
+          while ($row = mysqli_fetch_assoc($result_tasks)) { ?>
+            <tr>
+              <td><?php echo $row['dni']; ?></td>
+              <td><?php echo $row['nombres']; ?></td>
+              <td>
+                <div class="x-size"><?php echo $row['id_apuesta']; ?></div>
+              </td>
+              <td><?php echo $row['puntos']; ?></td>
+              <td>
+                <a href="edit.php?id=<?php echo $row['dni'] ?>" class="btn btn-secondary">
+                  <i class="fas fa-marker"></i>
+                </a>
+                <a href="delete_task.php?id=<?php echo $row['dni'] ?>" class="btn btn-danger">
+                  <i class="far fa-trash-alt"></i>
+                </a>
+              </td>
+            </tr>
           <?php } ?>
         </tbody>
       </table>
+      <style>
+        .x-size {
+          width: 300px;
+          overflow: auto;
+        }
+      </style>
     </div>
   </div>
 </main>
--- save_task.php
+++ save_task.php
@@ -16,23 +16,19 @@ if (isset($_POST['save_task'])) {
   $resultado1 = mysqli_query($conn, $condicion);
 
   if(mysqli_num_rows($resultado1) > 0){
-    /* $row = mysqli_fetch_array($resultado1);
-    $id_apuesta_mod = $id_apuesta . '|' . $row['id_apuesta']; */
-    $query = "UPDATE participante SET puntos = puntos + '$puntos' WHERE dni = $dni";
+    $row = mysqli_fetch_array($resultado1);
+    $id_apuesta_mod = $row['id_apuesta'] ."**". $id_apuesta; 
+    $query = "UPDATE participante SET puntos = puntos + '$puntos', id_apuesta = '$id_apuesta_mod' WHERE dni = $dni";
   }else{
     $query = "INSERT INTO participante(nombres, dni, id_apuesta, puntos) VALUES ('$nombres', '$dni', '$id_apuesta', '$puntos')";
   }
   
-
-
-
-  //$query = "SELECT CONCAT_WS('|', id_apuesta, $id_apuesta) AS id_apuesta FROM participante WHERE dni = $dni";
   $result = mysqli_query($conn, $query);
   if(!$result) {
     die("Query Failed.");
   }
 
-  $_SESSION['message'] = 'Ingreso satisfactorio';
+   $_SESSION['message'] = 'Ingreso satisfactorio';
   $_SESSION['message_type'] = 'success';
   header('Location: index.php');
 
